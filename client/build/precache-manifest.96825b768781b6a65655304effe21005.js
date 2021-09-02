c0016ab4a4afcf3751b51117b05ce3c352db570e
bscottalexander <bscottalexander@gmail.com>
2021-09-01 21:34:37 -0500

    BUILD FILES

---
+++ client/build/precache-manifest.96825b768781b6a65655304effe21005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85dbcac8073e1e948c047805f90a4410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fa0c9bcb8771cad3eb",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "5cb50f82aebf5a7758a2",
+    "url": "/static/js/2.be3ed0f0.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.be3ed0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fa0c9bcb8771cad3eb",
+    "url": "/static/js/main.1d5b7d4e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
