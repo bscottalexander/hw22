f0b4550474b9f88eefa8d8d10e21366f78859ee5
bscottalexander <bscottalexander@gmail.com>
2021-09-01 21:40:40 -0500

    UPDATE URL

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect('mongodb+srv://bscottalexander:Zyborn123@cluster0.g0jbq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
